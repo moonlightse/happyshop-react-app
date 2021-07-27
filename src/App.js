b4fa70c5202d258dbd3c452f4d802e71a7258e03
linhtnlK <linht.to@katalon.com>
2021-07-28 01:04:04 +0700

    login page

--- src/App.js
+++ src/App.js
@@ -7,13 +7,16 @@ import {
   Link,
   RouteWithSubRoutes,
 } from "react-router-dom";
+import Login from './pages/Login';
 
 function App() {
   return (
    
     <Router>
     <Switch>
-      <Route></Route>
+      <Route path='/' exact={true} component={Login}>
+
+      </Route>
     </Switch>
   </Router>
   );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,87 @@
-import React from 'react'
+import React,{useState} from 'react'
+import request from '../connect/AxiosCofig';
+import { Form, Input, Button, Checkbox } from 'antd';
 
-const Login =()=> {
-    return (<div>hi</div>)
-}
+const Login = () => {
+  const onFinish = async(values) => {
+    console.log('Success:', values.username);
+    const data = await request.get('');
+    console.log(data)
+  };
+  const [password,setPassword]=useState('pw')
+  const [username,setUsername]=useState('un')
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  return (
+    <Form
+      style={{margin:130}}
+      name="basic"
+      labelCol={{
+        span: 8,
+      }}
+      wrapperCol={{
+        span: 8,
+      }}
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+      
+      onFinishFailed={onFinishFailed}
+
+    >
+      <Form.Item
+        label="Username"
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your username!',
+          },
+        ]}
+      >
+        <Input/>
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your password!',
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        name="remember"
+        valuePropName="checked"
+        wrapperCol={{
+          offset: 8,
+          span: 10,
+        }}
+      >
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
+
+      <Form.Item
+        wrapperCol={{
+          offset: 8,
+          span: 10,
+        }}
+      >
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+
 export default Login
